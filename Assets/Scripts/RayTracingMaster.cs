e0b84641d9869e2c4b3da0680410805a7daaeb2b
miguelguti <miguelgutierrezruano@gmail.com>
2023-02-18 19:46:52 +0100

    compute shader now renders texture as skybox

--- Assets/Scripts/RayTracingMaster.cs
+++ Assets/Scripts/RayTracingMaster.cs
@@ -6,11 +6,29 @@ using UnityEngine;
 public class RayTracingMaster : MonoBehaviour
 {
     [SerializeField] private ComputeShader rayTracingShader;
+    [SerializeField] private Texture skyboxTexture;
 
     private RenderTexture target;
 
+    private Camera _camera;
+
+    private void Awake()
+    {
+        _camera = GetComponent<Camera>();
+    }
+
+    // Set the matrices of the camera on the shader
+    private void SetShaderParameters()
+    {
+        rayTracingShader.SetMatrix("_CameraToWorld", _camera.cameraToWorldMatrix);
+        rayTracingShader.SetMatrix("_CameraInverseProjection", _camera.projectionMatrix.inverse);
+        
+        rayTracingShader.SetTexture(0, "_SkyboxTexture", skyboxTexture);
+    }
+
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
+        SetShaderParameters();
         Render(destination);
     }
 
@@ -24,6 +42,7 @@ public class RayTracingMaster : MonoBehaviour
         int threadGroupsX = Mathf.CeilToInt(Screen.width / 8.0f);
         int threadGroupsY = Mathf.CeilToInt(Screen.height / 8.0f);
         rayTracingShader.Dispatch(0, threadGroupsX, threadGroupsY, 1);
+        
         // Blit the result texture to the screen
         Graphics.Blit(target, destination);
     }
