f64860d52f2a35a89f6da3033049210a1673098d
esne <miguelgutierrezruano@gmail.com>
2023-02-22 17:05:07 +0100

    added random materials to spheres
    passed to compute shader from list to buffer

--- Assets/Scripts/RayTracingMaster.cs
+++ Assets/Scripts/RayTracingMaster.cs
@@ -15,6 +15,13 @@ public class RayTracingMaster : MonoBehaviour
     [SerializeField] private Texture skyboxTexture;
     [SerializeField] private Light directionalLight;
 
+    [Header("Sphere parameters")]
+    [SerializeField] Vector2 SphereRadius = new Vector2(3.0f, 8.0f);
+    [SerializeField] uint SpheresMax = 100;
+    [SerializeField] float SpherePlacementRadius = 50.0f;
+
+    private ComputeBuffer _sphereBuffer;
+
     private RenderTexture target;
 
     private Camera _camera;
@@ -22,6 +29,71 @@ public class RayTracingMaster : MonoBehaviour
     // Antialiasing
     private uint _currentSample = 0;
     private Material _addMaterial;
+
+    struct Sphere
+    {
+        public Vector3 position;
+        public float radius;
+        public Vector3 albedo;
+        public Vector3 specular;
+    };
+
+    private void OnEnable()
+    {
+        _currentSample = 0;
+        SetUpScene();
+    }
+
+    private void OnDisable()
+    {
+        if (_sphereBuffer != null)
+            _sphereBuffer.Release();
+    }
+
+    private void SetUpScene()
+    {
+        List<Sphere> spheres = new List<Sphere>();
+
+        // Add a number of random spheres
+        for (int i = 0; i < SpheresMax; i++)
+        {
+            Sphere sphere = new Sphere();
+
+            // Radius and radius
+            sphere.radius = SphereRadius.x + Random.value * (SphereRadius.y - SphereRadius.x);
+
+            Vector2 randomPos = Random.insideUnitCircle * SpherePlacementRadius;
+            sphere.position = new Vector3(randomPos.x, sphere.radius, randomPos.y + SpherePlacementRadius * 2);
+
+            bool valid = true;
+
+            // Reject spheres that are intersecting others
+            foreach (Sphere other in spheres)
+            {
+                float minDist = sphere.radius + other.radius;
+                if (Vector3.SqrMagnitude(sphere.position - other.position) < minDist * minDist)
+                {
+                    valid = false;
+                    break;
+                }
+            }
+
+            if (!valid) continue;
+
+            // Albedo and specular color
+            Color color = Random.ColorHSV();
+            bool metal = Random.value < 0.2f;
+            sphere.albedo = metal ? Vector3.zero : new Vector3(color.r, color.g, color.b);
+            sphere.specular = metal ? new Vector3(color.r, color.g, color.b) : Vector3.one * 0.04f;
+
+            // Add the sphere to the list
+            spheres.Add(sphere);
+        }
+
+        // Assign to compute buffer
+        _sphereBuffer = new ComputeBuffer(spheres.Count, 40); // 40 is stride - byte lenght of struct
+        _sphereBuffer.SetData(spheres);
+    }
 
     private void Awake()
     {
@@ -60,7 +132,7 @@ public class RayTracingMaster : MonoBehaviour
         rayTracingShader.SetVector("_DirectionalLight", 
             new Vector4(lightDirection.x, lightDirection.y, lightDirection.z, directionalLight.intensity));
 
-
+        rayTracingShader.SetBuffer(0, "_Spheres", _sphereBuffer);
     }
 
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
